69a2235b9b3e62ef94dbb180f486c1d95ed2fab3
gstouten1 <guus.stouten@outlook.com>
2024-11-22 15:56:19 +0100

    Removed initial testing tests

--- test_example.py
+++ test_example.py
@@ -5,18 +5,6 @@ import pytest
 import main_biker_app
 from main_biker_app import *
 
-def multiply(a, b):
-    return a * b
-
-def test_multiply_positives():
-    assert multiply(2, 3) == 6  # Test case 1
-
-def test_multiply_negative():
-    assert multiply(-1, 1) == -1  # Test case 2
-
-def test_multiply_zero():
-    assert multiply(0, 5) == 0   # Test case 3
-
 @pytest.mark.parametrize("input,expected", [
     ("guus", False),
     ("guus@guus", False),
